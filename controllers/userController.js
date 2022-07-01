d7de3177d24365e6d368171d011b1a5a42858ecb
Ayami Higashi <ayamibhigashi@gmail.com>
2022-06-30 20:16:50 -0700

    add routes for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,58 @@
-const { User } = require('../models');
+const { User, Thought } = require("../models");
 
 module.exports = {
-    getAllUsers(req, res) {
-        User.find()
-        .then((users) => res.json(users))
-        .catch((err) => {
-            console.error(err)
-            res.status(500).json(err)
-        })
+  getAllUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+      });
+  },
+  // TODO: get user id to populate thought and friend data only
+  async getUserById(req, res) {
+    try {
+      const user = await User.findById(req.params.id);
+      if ((user.thoughts?.length ?? 0) > 0) {
+        user.thoughts = await Thought.find({
+          _id: { $in: user.thoughts },
+        });
+      }
+      res.json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
     }
-}
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+      });
+  },
+  updateUser(req, res) {
+    User.updateOne(
+      {
+        _id: req.params.id,
+      },
+      req.body
+    )
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteUser(req, res) {
+    User.deleteOne({
+        _id : req.params.id
+    })
+    .then((user) => res.json(user))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+  }
+};
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,5 +2,10 @@ const router = require('express').Router();
 const userController = require('../../controllers/userController')
 
 router.route('/').get(userController.getAllUsers)
+// TODO: get user id to populate thought and friend data only
+router.route('/:id').get(userController.getUserById)
+router.route('/').post(userController.createUser)
+router.route('/:id').put(userController.updateUser)
+router.route('/:id').delete(userController.deleteUser)
 
 module.exports = router;
