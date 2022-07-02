5b2405b1b063e732289d199d0eb7a2f9dc772ada
Ayami Higashi <ayamibhigashi@gmail.com>
2022-07-01 22:44:40 -0700

    add thought routes and endpoints for get, post, put, and delete

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,5 +8,46 @@ module.exports = {
           console.error(err);
           res.status(500).json(err);
         });
-    }
+    },
+    async getThoughtsById(req, res) {
+        try {
+          const thought = await Thought.findById(req.params.id);
+          res.json(thought);
+        } catch (err) {
+          console.error(err);
+          res.status(500).json(err);
+        }
+      },
+      async createThought(req, res) {
+        //TODO: push the created thought's _id to the associated user's thoughts array field
+        Thought.create(req.body)
+        .then((thought) => res.json(thought))
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json(err);
+        });
+      },
+      updateThought(req, res) {
+        Thought.updateOne(
+        {
+            _id: req.params.id,
+        },
+          req.body
+        )
+          .then((thought) => res.json(thought))
+          .catch((err) => {
+            console.error(err);
+            res.status(500).json(err);
+          });
+      },
+      deleteThought(req, res) {
+        Thought.deleteOne({
+            _id : req.params.id
+        })
+        .then((thought) => res.json(thought))
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json(err);
+        });
+      }
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,5 +2,9 @@ const router = require('express').Router();
 const thoughtController = require('../../controllers/thoughtController')
 
 router.route('/').get(thoughtController.getAllThoughts)
+router.route('/:id').get(thoughtController.getThoughtsById)
+router.route('/').post(thoughtController.createThought)
+router.route('/:id').put(thoughtController.updateThought)
+router.route('/:id').delete(thoughtController.deleteThought)
 
 module.exports = router;
